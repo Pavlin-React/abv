a7d0f7ae9dc3b492e1084e21c6c4c219aa2fa938
Pavlin Ivanov <pavlinivanov9@gmail.com>
2021-11-03 14:11:26 +0200

    init

---
+++ App.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import {View, Text, SafeAreaView, TouchableOpacity} from 'react-native';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import Gaming from './assets/gaming.svg';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+let App = () => {
+  return(
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Main' component={Main} options={{headerShown: false}}  />
+        <Stack.Screen name='Home' component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+
+const Main = ({navigation}) => {
+  return (
+    <SafeAreaView
+      style={{
+        backgroundColor: 'white',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <View style={{marginTop: 30}} >
+        <Text style={{fontSize: 30, fontWeight: 'bold', color: '#20315f'}}>
+          GAMEON
+        </Text>
+      </View>
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Gaming
+          width={300}
+          height={300}
+          style={{transform: [{rotate: '-15deg'}]}}
+        />
+      </View>
+      <TouchableOpacity
+        onPress={() => navigation.navigate('Home')}
+        style={{
+          backgroundColor: '#ad40af',
+          padding: 20,
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          width: '90%',
+          borderRadius: 5,
+          marginBottom: 50
+        }}>
+        <Text style={{color: 'white', fontSize: 22, fontWeight: 'bold'}}>
+          Let's Begin
+        </Text>
+        <MaterialIcons name="arrow-forward-ios" size={22} color="white" />
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+};
+
+let Home = ({navigation}) => {
+  return(
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}} >
+      <Text>This is Home Screen</Text>
+      <TouchableOpacity onPress={() => navigation.goBack()} >
+        <View>
+        <Text>Go to main Screen</Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+export default App;
