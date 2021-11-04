c5d88b2df9c2855f639f8ec5e1cf771570c35d28
Pavlin Ivanov <pavlinivanov9@gmail.com>
2021-11-04 11:37:11 +0200

    carousel

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   View,
   Text,
@@ -8,11 +8,27 @@ import {
   TextInput,
   TouchableOpacity,
 } from 'react-native';
+import Carousel from 'react-native-snap-carousel';
 import Feather from 'react-native-vector-icons/Feather';
 
-const HomeScreen = () => {
+import BannerSlider from '../components/BannerSlider';
+import {windowWidth} from '../utils/Dimensions';
+
+import {sliderData} from '../model/data';
+
+export default function HomeScreen({navigation}) {
+  const [gamesTab, setGamesTab] = useState(1);
+
+  const renderBanner = ({item, index}) => {
+    return <BannerSlider data={item} />;
+  };
+
+  const onSelectSwitch = value => {
+    setGamesTab(value);
+  };
+
   return (
-    <SafeAreaView style={{flex: 1, backgroundColor: 'white'}}>
+    <SafeAreaView style={{flex: 1, backgroundColor: '#fff'}}>
       <ScrollView style={{padding: 20}}>
         <View
           style={{
@@ -20,44 +36,61 @@ const HomeScreen = () => {
             justifyContent: 'space-between',
             marginBottom: 20,
           }}>
-          <Text style={{fontSize: 16}}>Hello Steve Jobs</Text>
+          <Text style={{fontSize: 18}}>
+            Hello John Doe
+          </Text>
+          <TouchableOpacity onPress={()=>navigation.goBack()}>
           <ImageBackground
             source={require('../assets/images/user-profile.jpg')}
             style={{width: 35, height: 35}}
             imageStyle={{borderRadius: 25}}
           />
+          </TouchableOpacity>
         </View>
+
         <View
           style={{
             flexDirection: 'row',
-            borderColor: '#c6c6c6',
+            borderColor: '#C6C6C6',
             borderWidth: 1,
-            padding: 10,
-            borderRadius: 7
+            borderRadius: 8,
+            paddingHorizontal: 10,
+            paddingVertical: 8,
           }}>
           <Feather
             name="search"
             size={20}
-            color="#c6c6c6"
+            color="#C6C6C6"
             style={{marginRight: 5}}
           />
           <TextInput placeholder="Search" />
         </View>
+
         <View
           style={{
+            marginVertical: 15,
             flexDirection: 'row',
             justifyContent: 'space-between',
-            marginVertical: 20,
-          }}
-        >
-          <Text style={{fontSize: 18, fontWeight: 'bold'}}>Upcoming Games</Text>
-          <TouchableOpacity onPress={() => {}} >
-          <Text style={{color: '#0aada8'}} >see all</Text>
+          }}>
+          <Text style={{fontSize: 18}}>
+            Upcoming Games
+          </Text>
+          <TouchableOpacity onPress={() => {}}>
+            <Text style={{color: '#0aada8'}}>See all</Text>
           </TouchableOpacity>
         </View>
+
+        <Carousel
+          ref={c => {
+            this._carousel = c;
+          }}
+          data={sliderData}
+          renderItem={renderBanner}
+          sliderWidth={windowWidth - 40}
+          itemWidth={300}
+          loop={true}
+        />
       </ScrollView>
     </SafeAreaView>
   );
-};
-
-export default HomeScreen;
+}
--- src/utils/Dimensions.js
+++ src/utils/Dimensions.js
@@ -1,3 +1,3 @@
-import { Dimensions } from "react-native";
-export let windowWith = Dimensions.get('window').width
-export let windowHeight = Dimensions.get('window').height
+import { Dimensions } from 'react-native';
+export const windowWidth = Dimensions.get('window').width;
+export const windowHeight = Dimensions.get('window').height;
